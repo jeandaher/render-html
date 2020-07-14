00c7f00b44de1b2cd716dad66406fee0d8d78e71
jeandaher <jeandaher1@gmail.com>
2020-07-14 21:31:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
+
+//__dirname : It will resolve to your project folder.
+router.get('/login',function(req,res){
+  res.sendFile(path.join(__dirname+'/login.html')); 
+}); 
+
+router.get('/form',function(req,res){
+    res.sendFile(path.join(__dirname+'/form.html'));
+  });
+
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+//add the router
+app.use('/', router); 
+app.use('/', router);
+app.listen(3007);
+console.log('Your Node Server is listening on '+app.get('port')); 
+/*
+app.listen(app.get('port'),  err => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log('Your Node Server is listening on '+app.get('port')); 
+});
+*/
